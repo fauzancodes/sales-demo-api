d42b482297c6b11563c8c08735dd328a1aa44f8d
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-09-30 00:07:06 +0700

    add whole app structure

--- build.sh
+++ build.sh
@@ -12,4 +12,7 @@ go build -o bin/ cmd/product/product.go
 echo "Building Customer Service  ..."
 go build -o bin/ cmd/customer/customer.go
 
+echo "Building Main Service  ..."
+go build -o bin/
+
 echo "Build completed!"
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/fauzancodes/sales-demo-api/app/config"
+	"github.com/fauzancodes/sales-demo-api/app/routes"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	app := echo.New()
+
+	config.Database()
+
+	routes.IndexRoute(app)
+	routes.AuthRoute(app)
+	routes.ProductRoute(app)
+	routes.CustomerRoute(app)
+
+	port := config.LoadConfig().IndexPort
+
+	log.Printf("Server: " + config.LoadConfig().BaseURL + ":" + port)
+	app.Logger.Fatal(app.Start(":" + port))
+}
