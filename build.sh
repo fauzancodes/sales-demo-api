a18e9b0537cc94fc9d7eab0a03234ac646daa288
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-10-06 17:39:49 +0700

    add build sale service in build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ go build -o bin/ cmd/product/product.go
 echo "Building Customer Service  ..."
 go build -o bin/ cmd/customer/customer.go
 
+echo "Building Sale Service  ..."
+go build -o bin/ cmd/sale/sale.go
+
 echo "Building Main Service  ..."
 go build -o bin/
 
