8ff95a06e942df2a81eaeccd133f8298ec130afc
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-11-21 20:21:38 +0700

    add vercel.json

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
-package main
+package handler //for vercel
+// package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/fauzancodes/sales-demo-api/app/config"
 	"github.com/fauzancodes/sales-demo-api/app/middlewares"
@@ -17,6 +19,12 @@ func main() {
 
 	log.Printf("Server: " + config.LoadConfig().BaseUrl + ":" + port)
 	app.Logger.Fatal(app.Start(":" + port))
+}
+
+func Main(w http.ResponseWriter, r *http.Request) {
+	e := Init()
+
+	e.ServeHTTP(w, r)
 }
 
 func Init() *echo.Echo {
